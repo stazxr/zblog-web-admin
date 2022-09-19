4177987d3dfeaff3e871d5741ae15ae1c7be9701
suntao <stazxr@qq.com>
2022-09-19 14:14:52 +0800

    路由管理功能开发

--- src/api/base/router.js
+++ src/api/base/router.js
@@ -1,8 +1,14 @@
 import api from '../custom-axios'
 
+const routerApi = '/api/router'
+
 export default {
+  // 分页查询角色列表
+  pageRouterList: params => {
+    return api.httpRequest().get(`${routerApi}/pageList`, params)
+  },
   // 根据权限编码查询路由信息
   queryRouterByCode: params => {
-    return api.httpRequest().get(`/api/router/queryRouterByCode`, params)
+    return api.httpRequest().get(`${routerApi}/queryRouterByCode`, params)
   }
 }
