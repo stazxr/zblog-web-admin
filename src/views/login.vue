4dc80cae52c6e27c3581b1a9b5a049bad3bf306a
SunTao <1027353579@qq.com>
2023-02-21 17:25:49 +0800

    登录组件的样式增加scoped属性，防止属性污染

--- src/views/login.vue
+++ src/views/login.vue
@@ -168,7 +168,7 @@ export default {
 }
 </script>
 
-<style rel="stylesheet/scss" lang="scss">
+<style rel="stylesheet/scss" lang="scss" scoped>
   .login {
     display: flex;
     justify-content: center;
