8f45168c963c110a6dec34f7f4bcbaba42bc85a9
SunTao <1027353579@qq.com>
2024-04-19 01:27:16 +0800

    创建缪斯框架 musesFrame 专用目录

---
+++ src/components/muses/MusesDialog/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-dialog :title="title" :visible.sync="dialogVisible">
+    <slot />
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    dialogVisible: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
